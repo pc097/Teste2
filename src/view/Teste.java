d82493f5c76d3f75b93f604de3ae370d4fde6700
Renato Braga <renatoebraga@hotmail.com>
2020-12-21 21:24:26 -0300

    teste

---
+++ src/view/Teste.java
@@ -0,0 +1,13 @@
+package view;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		int base = 4;
+		int altura = 3;
+		float area = base * altura / 2;
+		System.out.println("A área do traingulo é " + area);
+	}
+
+}
